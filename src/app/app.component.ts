467186f674151989ef98b4c9fb2f841f821153ab
eduardosm123 <magalhaeseduardo91@gmail.com>
2025-03-30 15:23:19 -0300

    add: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core'; 
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CalculadoraComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
